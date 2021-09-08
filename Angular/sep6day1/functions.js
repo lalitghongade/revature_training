7e9fb88cc543b6d71e2344470292484641d655b0
lalit ghongade <lalitghongade1451999@gmail.com>
2021-09-08 22:09:50 +0530

    function practice

--- Angular/sep6day1/functions.js
+++ Angular/sep6day1/functions.js
@@ -5,3 +5,16 @@ function add(num1, num2) {
 }
 var sum = add(5);
 console.log(sum);
+function fullname(person) {
+    console.log(person.fname + " " + person.lname);
+}
+var p = {
+    fname: "jack",
+    lname: "sparrow"
+};
+fullname(p); //clumsy code code maintenance also difficult
+function fullnameImprovised(person) {
+    console.log("improvised version with using interface");
+    console.log(person.fname + " " + person.lname);
+}
+fullnameImprovised(p);
--- Angular/sep6day1/functions.ts
+++ Angular/sep6day1/functions.ts
@@ -8,3 +8,31 @@ function add(num1:number,num2?:number): number//retuern type
 
 let sum =add(5);
 console.log(sum);
+
+
+function fullname(person :{fname:string,lname:string}){
+    console.log(`${person.fname} ${person.lname}`);
+}
+
+let p ={
+    fname:"jack",
+    lname:"sparrow"
+};
+
+fullname(p);//clumsy code code maintenance also difficult
+
+
+interface Person{
+    fname:string;
+    lname:string;
+}
+
+
+
+function fullnameImprovised(person : Person){
+    console.log("improvised version with using interface");
+    console.log(`${person.fname} ${person.lname}`);
+}
+
+
+fullnameImprovised(p);
