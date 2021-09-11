c949644c123de95922c5c25c4c6f78680b4de76f
lalit ghongade <lalitghongade1451999@gmail.com>
2021-09-11 11:56:13 +0530

    department component added

---
+++ Angular/sep9day4/routingDemo/src/app/department-list/department-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-department-list',
+  template: `
+    <div style="text-align=center">
+    <p>
+      department-list works!
+    </p>
+</div>
+`,
+  styles: []
+})
+export class DepartmentListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
